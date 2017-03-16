4b7b0c4a93be3ccd7e88972a470ba47f50e9469c
Eric Secules <esecules@gmail.com>
2017-03-16 10:47:39 -0700

    fixing up install script

--- install.sh
+++ install.sh
@@ -5,15 +5,35 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-read -p "how often to change the background? (minutes): " minutes
+if ! which anacron; then
+    echo "anacron must be installed"
+    exit 1
+fi
 
+
+read -p "how often to change the background? (minutes): " minutes
 if ((60 % $minutes)) || [ 60 -le $minutes ]; then
     echo "minutes must be divisible by and less than 60"
     exit 1
 fi
 
+read -p "subreddit to get the images from, default 'wallpaper': " subreddit
+
+if [ -z $subreddit ]; then
+    echo "using default subreddit"
+    subreddit='wallpaper'
+else
+    echo "Trusting that the $subreddit subreddit exists"
+fi
+
+mkdir -p /home/$SUDO_USER/Pictures/wallpaper /home/$SUDO_USER/Pictures/saved_wallpaper
+
 echo "copying files to usr/local/bin"
 cp wallpaperSwitcher wpdownload wprand /usr/local/bin 
+cp anacrontab /etc/cron.daily
+
+sed -ri "s/subreddit='wallpaper'/subreddit='$subreddit'/g" /usr/local/bin/wpdownload
+sed -ri "s/user='username'/user='$SUDO_USER'/g" /usr/local/bin/wpdownload
 
 echo "adding line to user $SUDO_USER crontab"
 line="*/$minutes * * * * . wallpaperSwitcher"
